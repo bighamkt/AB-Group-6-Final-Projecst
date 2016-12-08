3122095c90fe9d7f561c69324e87e838de1a1358
mattpow <main@mattpow.net>
2016-12-07 17:29:09 -0800

    Added project proposal to home page

--- ui.r
+++ ui.r
@@ -17,10 +17,27 @@ $(document).keyup(function(event) {
 
 shinyUI(navbarPage(title = "IMDB Dataset",
    theme = "boostrap.css",
-   tabPanel("Home"
-      
-   
-      
+   tabPanel("Home",
+      HTML("<h1><u>Project Proposal</u><h1>"),
+      h3("Project Description"),
+      p("We will be working with an IMDB dataset from Kaggle. The dataset was created by Kaggle user chuansun76. They created it by scraping IMDB. Allowing them to accumulate 28 variables for 5043 movies, 
+        that span across 100 years and 66 countries. Some of the variables include the movie’s title, director, actors, critic reviews, user reviews, and genres. The dataset can be downloaded from Kaggle website
+        (https://www.kaggle.com/deepmatrix/imdb-5000-movie-dataset). Our target audience is movie lovers who is trying to find good movies based on his or her interests. Some specific questions our audience will 
+        be able to answer from our project will be:"),
+      HTML('
+            <ol>
+               <li>What are the best rated movies in certain genres?</li>
+               <li>What are the movies that have audience’s two favorite actors?</li>
+               <li>What are the movies that have the most popular actors?</li>
+            </ol>
+           '),
+      h3("Technical Description"),
+      p("We’ll be formating our final project as an HTML page. Our data will be read in using the .CSV file downloaded from Kaggle. Some reshaping will be necessary for our project such as rearranging the movies 
+        based on genre since one movie could have many genres, as well as trimming down some of the unnecessary variables since we won’t be using all 28 available in the original dataset. We plan on using the 
+        libraries we’ve used up until now such as dplyr and plotly. We don’t currently plan on using any new libraries but we will research them as we run into issues that require additional capabilities. We’ll 
+        be using some basic statistical analysis but don’t anticipate needing to use anything beyond the basics or machine learning. The biggest challenge we foresee is maintaining a vision for this project in 
+        the face of such a large dataset. Since we have so many variables available to investigate we will need to maintain strong communication to make sure that everyone is on the same page about which questions 
+        we are focusing on.")
    ),
    tabPanel("Ratings"
       
